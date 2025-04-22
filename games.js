ebf1f0e662013795b8250516eb0176bf813fae79
Jack <burrowsjack1502@gmail.com>
2025-04-22 17:19:14 +1000

    Update games.js

--- games.js
+++ games.js
@@ -4,36 +4,42 @@ const games = [
         title: "2048",
         description: "A popular sliding tile puzzle game",
         image: "https://raw.githubusercontent.com/[YOUR_USERNAME]/unblockedGames/main/gameimages/2048.png",
-        url: "https://play2048.co/"
+        url: "https://play2048.co/",
+        category: "Puzzle"
     },
     {
         title: "Snake",
         description: "Classic snake game",
         image: "https://raw.githubusercontent.com/[YOUR_USERNAME]/unblockedGames/main/gameimages/snake.png",
-        url: "https://playsnake.org/"
+        url: "https://playsnake.org/",
+        category: "Classic"
     },
     {
         title: "Tetris",
         description: "The classic block-stacking game",
         image: "https://raw.githubusercontent.com/[YOUR_USERNAME]/unblockedGames/main/gameimages/tetris.png",
-        url: "https://tetris.com/play-tetris"
+        url: "https://tetris.com/play-tetris",
+        category: "Classic"
     },
     {
         title: "Pac-Man",
         description: "Classic arcade game",
         image: "https://raw.githubusercontent.com/[YOUR_USERNAME]/unblockedGames/main/gameimages/pacman.png",
-        url: "https://www.pacman.com/"
+        url: "https://www.pacman.com/",
+        category: "Classic"
     },
     {
         title: "Minesweeper",
         description: "Classic puzzle game",
         image: "https://raw.githubusercontent.com/[YOUR_USERNAME]/unblockedGames/main/gameimages/minesweeper.png",
-        url: "https://minesweeper.online/"
+        url: "https://minesweeper.online/",
+        category: "Puzzle"
     },
     {
         title: "Chess",
         description: "Play chess online",
         image: "https://raw.githubusercontent.com/[YOUR_USERNAME]/unblockedGames/main/gameimages/chess.png",
-        url: "https://www.chess.com/play/computer"
+        url: "https://www.chess.com/play/computer",
+        category: "Strategy"
     }
 ]; 
