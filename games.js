c053f5c0e527ef608355d28858d73725653fe2aa
Jack <burrowsjack1502@gmail.com>
2025-04-22 19:10:05 +1000

    Update games.js

--- games.js
+++ games.js
@@ -5,20 +5,27 @@ const games = [
         description: "Tip over the other sheep!",
         image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/crazycattle3d.png?raw=true",
         url: "https://crazy-cattle3d.org/game/crazycattle3d//",
-        category: "Game"
+        category: "Action"
     },
     {
         title: "Slowroads.io",
         description: "Endless Driving Zen",
         image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/slowroads.png?raw=true",
         url: "https://slowroads.io",
-        category: "Game"
+        category: "Casual"
     },
     {
         title: "Monkey Mart",
         description: "Classic Monkey Money Game",
         image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/monkeymart.png?raw=true",
         url: "https://hushygames.github.io/games/monkeymart/game.html",
-        category: "Classic"
-    }
-]; 
+        category: "Simulation"
+    },
+    {
+        title: "Slope",
+        description: "Slide down a hill!",
+        image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/slope.png?raw=true",
+        url: "https://gamedump.github.io/slope/",
+        category: "Action"
+    {
+    ]; 
