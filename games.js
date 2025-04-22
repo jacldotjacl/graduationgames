c7280126208ec65847746bc01fc9988cd2d93a52
Jack <burrowsjack1502@gmail.com>
2025-04-22 18:27:29 +1000

    Update games.js

--- games.js
+++ games.js
@@ -3,14 +3,14 @@ const games = [
     {
         title: "Crazy Cattle 3D",
         description: "Tip over the other sheep!",
-        image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/2048.png?raw=true",
+        image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/crazycattle3d.png",
         url: "https://unblockyunblocky.github.io/oopsyoufoundmegames/crazycattle/",
         category: "Game"
     },
     {
         title: "Slowroads.io",
         description: "Endless Driving Zen",
-        image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/snake.png?raw=true",
+        image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/slowroads.png",
         url: "https://slowroads.io",
         category: "Game"
     },
