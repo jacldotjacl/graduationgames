3d90d236dfbe065e6ac9521f73781c52124aed98
Jack <burrowsjack1502@gmail.com>
2025-04-23 22:55:57 +1000

    Update games.js

--- games.js
+++ games.js
@@ -111,5 +111,145 @@ const games = [
         image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/roundracers.png?raw=true",
         url: "https://physicsedu16.github.io/g97/class-551/",
         category: "Racing"
-    }
+    },
+    {
+    title: "Papa's Freezeria",
+    description: "Serve cool frozen treats.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/freezeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-freezeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Burgeria",
+    description: "Grill, stack, and serve.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/burgeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-burgeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Pizzeria",
+    description: "Top, bake, slice, repeat.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/pizzeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-pizzeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Wingeria",
+    description: "Fry and sauce wings.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/wingeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-wingeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Sushiria",
+    description: "Roll up sushi fast.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/sushiria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-sushiria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Pastaria",
+    description: "Boil pasta and sauce.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/pastaria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-pastaria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Cheeseria",
+    description: "Make grilled cheese meals.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/cheeseria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-cheeseria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Cupcakeria",
+    description: "Bake and decorate cupcakes.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/cupcakeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-cupcakeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Donuteria",
+    description: "Fry, glaze, and serve.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/donuteria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-donuteria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Taco Mia",
+    description: "Grill and wrap tacos.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/tacomia.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-taco-mia/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Bakeria",
+    description: "Fill and bake pies.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/bakeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-bakeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Hotdoggeria",
+    description: "Grill dogs, pour drinks.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/hotdoggeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-hot-doggeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Pancakeria",
+    description: "Stack pancakes just right.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/pancakeria.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-pancakeria/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa's Scooperia",
+    description: "Scoop, bake, and serve.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/scooperia.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papas-scooperia/",
+    category: "Cooking"
+  },
+  {
+    title: "Papa Louie: Pizzas Attack",
+    description: "Fight pizzas, rescue cooks.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/papalouie1.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papa-louie-1/",
+    category: "Action"
+  },
+  {
+    title: "Papa Louie 2: Burgers Attack",
+    description: "Save friends, dodge burgers.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/papalouie2.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papa-louie-2/",
+    category: "Action"
+  },
+  {
+    title: "Papa Louie 3: Sundaes Attack",
+    description: "Explore worlds, fight desserts.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/papalouie3.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/papa-louie-3/",
+    category: "Action"
+  },
+  {
+    title: "Jacksmith",
+    description: "Forge gear, lead battle.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/jacksmith.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/jacksmith/",
+    category: "Strategy"
+  },
+  {
+    title: "Cactus McCoy",
+    description: "Punch baddies as cactus.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/cactus1.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/cactus-mccoy-1/",
+    category: "Adventure"
+  },
+  {
+    title: "Cactus McCoy 2",
+    description: "New levels, more action.",
+    image: "https://github.com/jacldotjacl/graduationgames/blob/main/gameicons/cactus2.png?raw=true",
+    url: "https://papasgamesx.github.io/flipline/cactus-mccoy-2/",
+    category: "Adventure"
+  }
 ];
